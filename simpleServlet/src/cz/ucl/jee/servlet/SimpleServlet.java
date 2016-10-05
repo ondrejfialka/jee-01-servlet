76388bd2bde513b74f2a72a5370472fb49d3c53e
Ondra <Ondra>
2016-10-05 21:44:23 +0200

    initial commit

---
+++ simpleServlet/src/cz/ucl/jee/servlet/SimpleServlet.java
@@ -0,0 +1,28 @@
+package cz.ucl.jee.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("<h1><b>Hello World!</b></h1>"
+				+ "<br>This output was produced by servlet.");
+		writer.println("</body>");
+		writer.println("</html>");
+				
+		writer.close();
+	}
+	
+}
